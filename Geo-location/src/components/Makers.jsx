5e081963ac1976497e7089cc1a84e8dc224cda37
Javier Valladares Alonso <javierprofesionalwork@gmail.com>
2025-02-01 21:37:03 -0500

    Add files via upload

--- Geo-location/src/components/Makers.jsx
+++ Geo-location/src/components/Makers.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { Marker, Popup } from 'react-leaflet'
+import L from 'leaflet';
 
+const MyIcon = L.icon({
+  iconUrl: '../assets/venue_location_icon.svg', 
+  iconSize: [32, 32], 
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32] 
+});
 export default function Makers ({ location }) {
   return (
-    <Marker position={location}>
+    <Marker position={location} icon={MyIcon}>
       <Popup>Your localization</Popup>
     </Marker>
   )
